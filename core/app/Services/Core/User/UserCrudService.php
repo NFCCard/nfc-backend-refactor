2b4b78225a4815ef59a01925fb6390ed8195c5d9
mortazavi <s.mohammad027@live.com>
2022-09-15 15:42:19 +0430

    UserCrudService actions implemented;

--- core/app/Services/Core/User/UserCrudService.php
+++ core/app/Services/Core/User/UserCrudService.php
@@ -1,7 +1,47 @@
 <?php
 
-	namespace App\Services\Core\User;
+    namespace App\Services\Core\User;
 
-	class UserCrudService {
+    use App\Exceptions\Core\User\UserException;
+    use App\Models\Core\User;
+    use App\Repositories\Contracts\Core\IUserRepository;
 
-	}
+    class UserCrudService {
+        private IUserRepository $repository;
+
+        public function __construct() {
+            $this->repository = app( IUserRepository::class );
+        }
+
+        public function all(): Paginator {
+            return $this->repository->all()->applyFilters()->paginate();
+        }
+
+        public function find( int $model ): User {
+            return $this->repository->find( $model );
+        }
+
+        public function create( array $data ): User {
+            if ( $model = $this->repository->create( $data ) ) {
+                return $model;
+            }
+
+            throw UserException::failedToCreate();
+        }
+
+        public function update( User $model, array $data ): User {
+            if ( $this->repository->update( $model, $data ) ) {
+                return $model;
+            }
+
+            throw UserException::failedToUpdate();
+        }
+
+        public function delete( User $model ): User {
+            if ( $this->repository->delete( $model ) ) {
+                return $model;
+            }
+
+            throw UserException::failedToDelete();
+        }
+    }
