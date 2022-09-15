a635ddbeff123d0dc0ea8f1981dd17df014025ba
mortazavi <s.mohammad027@live.com>
2022-09-15 15:13:53 +0430

    RoleAndPermissionSeeder updated;

--- core/database/seeders/Core/RoleAndPermissionSeeder.php
+++ core/database/seeders/Core/RoleAndPermissionSeeder.php
@@ -18,51 +18,20 @@
          * @throws Throwable
          */
         public function run() {
-            Horus::createPermissions( [
-                // create basic permissions for given model plus additional permissions [ 'viewRole', 'viewAuthor' ]
-                // you can Model::class => 'permission' if you have one additional permission
-                User::class => [
-                    'viewHorizon',
-                ],
-            ], AreasEnum::ADMIN // you can create permissions for a specific guard
-            );
-            Horus::createPermissions( [
-                User::class
-            ], AreasEnum::USER );
-
             Horus::createRoles( [
-                // create roles
                 RolesEnum::DEFAULT_ADMINS
-            ], AreasEnum::ADMIN  // you can determine a guard for a set of roles. you can assign permissions
-            // with same guard to a role
+            ], AreasEnum::ADMIN
             );
-            Horus::createRoles( [ RolesEnum::DEFAULT_USERS ], AreasEnum::USER );
-
-            // assign permissions to the given roles
-            Horus::assignPermissionsToRole( Role::findByName( RolesEnum::DEFAULT_ADMINS, AreasEnum::ADMIN ), [
-                // you can Model::class => '*' if you just want basic permissions to assign to the role
-                // also you can Model::class => 'permission' if you have one permission for a role
-                User::class => [
-                    '*',
-
-                    'viewHorizon',
-                ]
-            ], AreasEnum::ADMIN // determine the permissions guard for assigning to a role
-            // ( *notice : role must be in same guard as permissions are )
-            );
-            Horus::assignPermissionsToRole( Role::findByName( RolesEnum::DEFAULT_USERS, AreasEnum::USER ), [
-                User::class => [ 'view' ]
+            Horus::createRoles( [
+                RolesEnum::DEFAULT_USERS
             ], AreasEnum::USER );
 
-            // super permissions
+
             Horus::createSuperPermissions( [
-                '*', // create *-* permission
-                User::class => '*', // create user-*
+                User::class => '*',
             ], AreasEnum::ADMIN );
 
-            // assign super permissions
             Horus::assignSuperPermissionsToRole( Role::findByName( RolesEnum::DEFAULT_ADMINS, AreasEnum::ADMIN ), [
-                '*',
                 User::class,
             ] );
 
