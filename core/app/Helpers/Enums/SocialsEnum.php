23b3804f8ffdbc60d024454b071d83f5a3dc4b78
mortazavi <s.mohammad027@live.com>
2022-09-17 15:01:27 +0430

    new social medias added;

--- core/app/Helpers/Enums/SocialsEnum.php
+++ core/app/Helpers/Enums/SocialsEnum.php
@@ -7,9 +7,18 @@
     enum SocialsEnum: string {
         use EnumHelper;
 
+        case GITHUB = 'github';
+        case INSTAGRAM = 'instagram';
         case TELEGRAM = 'telegram';
         case WHATSAPP = 'whatsapp';
-        case INSTAGRAM = 'instagram';
+        case Dribble = 'dribble';
         case LINKEDIN = 'linkedin';
-        case GITHUB = 'github';
+        case PINTEREST = 'pinterest';
+        case TWITTER = 'twitter';
+        case YOUTUBE = 'youtube';
+        case APARAT = 'apart';
+        case TIKTAK = 'tiktak';
+        case SPOTIFY = 'spotify';
+        case SOUNDCLOUD = 'soundcloud';
+        case TWITCH = 'twitch';
     }
