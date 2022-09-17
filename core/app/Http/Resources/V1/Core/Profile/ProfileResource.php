f9f155c1d934d48471ee3922dd42f93b0cd04ce5
mortazavi <s.mohammad027@live.com>
2022-09-17 21:27:34 +0430

    resource added to ProfileResource;

--- core/app/Http/Resources/V1/Core/Profile/ProfileResource.php
+++ core/app/Http/Resources/V1/Core/Profile/ProfileResource.php
@@ -4,6 +4,8 @@
 
     use App\Http\Resources\Traits\InteractsWithPivots;
     use App\Http\Resources\Traits\InteractsWithRelations;
+    use App\Http\Resources\V1\Core\Resource\ResourcesResource;
+    use App\Repositories\Contracts\Core\IProfileRepository;
     use Illuminate\Http\Request;
     use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -27,6 +29,7 @@
                 'first_name'  => $this->first_name,
                 'last_name'   => $this->last_name,
                 'description' => $this->description,
+                'resource'    => ResourcesResource::make( app( IProfileRepository::class )->viewResource( $this->resource ) ),
             ];
 
             return $data;
